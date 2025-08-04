046c58223523ee37a55021bd9d0e78bf3074ea64
LiamSnow <mail@liamsnow.com>
2025-08-04 15:32:52 -0400

    updated home page writing

--- src/home.rs
+++ src/home.rs
@@ -19,12 +19,11 @@ pub async fn get_home() -> Markup {
 
 const ABOUT_ME: &str = "
 I'm a Computer Science MS and Electrical & Computer Engineering BS student at
-Worcester Polytechnic Institute with a passion for building efficient, reliable systems.
+Worcester Polytechnic Institute with a passion for systems programming and backend
+development.
 I enjoy learning in all areas of CS and have experience in many languages:
 Rust, Golang, C, C++, Python, Java, C#, TypeScript, JavaScript, Lua, Swift, and others.
-However, my passion truly lies in writing Rust 🦀.
 ";
-
 
 pub fn make_home_html(
     recent_projects: Vec<(String, PostMeta)>,
@@ -59,45 +58,29 @@ pub fn make_home_html(
             }
 
             #sections {
-                section {
-                    .header {
-                        span { "/ SKILLS" }
-                        span { "■" }
-                    }
-                    .grid {
-                        div {
-                            h3 { "Systems Programming" }
-                            p {
-                                "I love writing fast, low-level code code, and especially working with binary protocols.
-                                Recently I have written the ESPHome protocol in rust ("
-                                a href="/projects/esphomebridge-rs" { "esphomebridge-rs" }
-                                ") and also made open source software for the Eightsleep ("
-                                a href="/projects/opensleep" { "opensleep" }
-                                ") which fully implements the undocumented protocols for temperature control and sleep tracking."
-                            }
-                        }
-                        div {
-                            h3 { "Backend/API" }
-                            p {
-                                "I've been writing backends and APIs from around the time
-                                I started programming. I've made them in many languages
-                                Rust (Axum, Actix), Go (Gin), .NET, FastAPI to name
-                                a few.
-                                I've worked with REST and WebSockets extensively and have
-                                some GraphQL experience.
-                                For databases, I've worked in many things too: SQL (PostgreSQL, SQLite, Amazon RDS) and NoSQL (GC Datastore, SledDB)"
-                            }
-                        }
-                        div {
-                            h3 { "Architecture" }
-                            p {
-                                "My approach to writing software starts with thoughfully
-                                planning out architecture, and evolving that architecture
-                                early on. I believe that this is the foundation of good software. When systems are well planned, everything else fall into place, bugs are easier to fix and harder to make in the first place. Furthermore, it becomes easier to add new features and work on with many engineers."
-                            }
-                        }
-                    }
-                }
+                // section {
+                //     .header {
+                //         span { "/ SKILLS" }
+                //         span { "■" }
+                //     }
+                //     .grid {
+                //         div {
+                //             h3 { "Systems Programming" }
+                //             p {
+                //                 "Networking, protocols, and concurrent systems. Check out "
+                //                 a href="/projects/esphomebridge-rs" { "esphomebridge-rs" }
+                //                 " & "
+                //                 a href="/projects/opensleep" { "opensleep" }
+                //             }
+                //         }
+                //         div {
+                //             h3 { "Backend Development" }
+                //             p {
+                //                 "Fast APIs, SQL, NoSQL, embedded databases, REST, WebSockets, GraphQL"
+                //             }
+                //         }
+                //     }
+                // }
 
                 section {
                     .header {
